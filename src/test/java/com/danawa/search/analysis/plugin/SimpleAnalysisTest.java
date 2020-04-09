da8edd496c5a3d7ebe8837f8e9d5c46221e65e9a
lupfeliz <lupfeliz@gmail.com>
2020-04-09 11:00:59 +0900

    패키지 정리
    1. gradle 빌드 스크립트 수정(미완)
    2. elasticsearch-nori 플러그인용 테스트 삭제
    3. SimpleAnalysisTest -> com.danawa 패키지로 이동

--- src/test/java/com/danawa/search/analysis/plugin/SimpleAnalysisTest.java
+++ src/test/java/com/danawa/search/analysis/plugin/SimpleAnalysisTest.java
@@ -1,4 +1,4 @@
-package org.elasticsearch.index.analysis;
+package com.danawa.search.analysis.plugin;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -13,14 +13,17 @@ import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
-
-import com.danawa.search.analysis.plugin.AnalysisProductNamePlugin;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.ko.KoreanAnalyzer;
 import org.apache.lucene.analysis.ko.KoreanTokenizer;
+import org.apache.lucene.analysis.ko.POS;
 import org.apache.lucene.analysis.ko.KoreanTokenizer.DecompoundMode;
+import org.apache.lucene.analysis.ko.POS.Tag;
 import org.apache.lucene.analysis.ko.dict.UserDictionary;
 import org.apache.lucene.analysis.ko.util.DictionaryBuilder;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
@@ -33,6 +36,8 @@ import org.elasticsearch.cluster.metadata.IndexMetaData;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.index.IndexSettings;
+import org.elasticsearch.index.analysis.AnalysisRegistry;
+import org.elasticsearch.index.analysis.IndexAnalyzers;
 import org.elasticsearch.indices.analysis.AnalysisModule;
 import org.elasticsearch.test.IndexSettingsModule;
 import org.junit.Test;
@@ -42,16 +47,18 @@ import org.slf4j.LoggerFactory;
 @LuceneTestCase.SuppressSysoutChecks(bugUrl = "")
 public class SimpleAnalysisTest extends LuceneTestCase {
     private static Logger logger = LoggerFactory.getLogger(SimpleAnalysisTest.class);
-    //private static final String text = "아름다운이땅에금수강산에단군할아버지가터잡으시고홍익인간뜻으로나라세우니대대손손훌륭한인물도많아고구려세운동명왕백제온조왕알에서나온혁거세만주벌판달려라광개토대왕신라장군이사부백결선생떡방아삼천궁녀의자왕황산벌의계백맞서싸운관창역사는흐른다말목자른김유신통일문무왕원효대사해골물혜초천축국바다의왕자장보고발해대조영귀주대첩강감찬서희거란족무단정치정중부화포최무선죽림칠현김부식지눌국사조계종의천천태종대마도정벌이종무일편단심정몽주목화씨는문익점해동공자최충삼국유사일연역사는흐른다황금을보기를돌같이하라최영장군의말씀받들자황희정승맹사성과학장영실신숙주와한명회역사는안다십만양병이율곡주리이퇴계신사임당오죽헌잘싸운다곽재우조헌김시민나라구한이순신태정태세문단세사육신과생육신몸바쳐서논개행주치마권율역사는흐른다번쩍번쩍홍길동의적임꺽정대쪽같은삼학사어사박문수삼년공부한석봉단원풍속도방랑시인김삿갓지도김정호영조대왕신문고정조규장각목민심서정약용녹두장군전봉준순교김대건서화가무황진이못살겠다홍경래삼일천하김옥균안중근은애국이완용은매국역사는흐른다별헤는밤윤동주종두지석영삼십삼인손병희만세만세유관순도산안창호어린이날방정환이수일과심순애장군의아들김두한날자꾸나이상황소그림중섭역사는흐른다";
-    private static final String text = "아름다운ABC이땅에123금수강산에a12단군할아버지가터잡으시고홍익인간뜻으로나라세우니대대손손훌륭한인물도많아고구려세운동명왕백제온조왕알에서나온혁거세";
+    //private static final String TEXT_STR = "아름다운이땅에금수강산에단군할아버지가터잡으시고홍익인간뜻으로나라세우니대대손손훌륭한인물도많아고구려세운동명왕백제온조왕알에서나온혁거세만주벌판달려라광개토대왕신라장군이사부백결선생떡방아삼천궁녀의자왕황산벌의계백맞서싸운관창역사는흐른다말목자른김유신통일문무왕원효대사해골물혜초천축국바다의왕자장보고발해대조영귀주대첩강감찬서희거란족무단정치정중부화포최무선죽림칠현김부식지눌국사조계종의천천태종대마도정벌이종무일편단심정몽주목화씨는문익점해동공자최충삼국유사일연역사는흐른다황금을보기를돌같이하라최영장군의말씀받들자황희정승맹사성과학장영실신숙주와한명회역사는안다십만양병이율곡주리이퇴계신사임당오죽헌잘싸운다곽재우조헌김시민나라구한이순신태정태세문단세사육신과생육신몸바쳐서논개행주치마권율역사는흐른다번쩍번쩍홍길동의적임꺽정대쪽같은삼학사어사박문수삼년공부한석봉단원풍속도방랑시인김삿갓지도김정호영조대왕신문고정조규장각목민심서정약용녹두장군전봉준순교김대건서화가무황진이못살겠다홍경래삼일천하김옥균안중근은애국이완용은매국역사는흐른다별헤는밤윤동주종두지석영삼십삼인손병희만세만세유관순도산안창호어린이날방정환이수일과심순애장군의아들김두한날자꾸나이상황소그림중섭역사는흐른다";
+    private static final String TEXT_STR = "아름다운ABC이땅에123금수강산에a12단군할아버지가터잡으시고홍익인간뜻으로나라세우니대대손손훌륭한인물도많아고구려세운동명왕백제온조왕알에서나온혁거세";
+    private static final String USER_DICT_STR = "아름다운,금수강산,홍익인간,a12,백제,온조왕,동명왕".replaceAll("[, ]", "\n");
+    private static final String STOP_TAG_STR = "NR,SP";
 
     @Test
     public void longTextTest() throws Exception {
         final Settings settings = Settings.builder().put("index.analysis.analyzer.my_analyzer.type", "product_name")
-                .put("index.analysis.analyzer.my_analyzer.stoptags", "NR, SP")
+                .put("index.analysis.analyzer.my_analyzer.stoptags", STOP_TAG_STR)
                 .put("index.analysis.analyzer.my_analyzer.decompound_mode", "mixed").build();
         final Analyzer analyzer = createTestAnalysis(settings);
-        final TokenStream stream = analyzer.tokenStream("", text);
+        final TokenStream stream = analyzer.tokenStream("", TEXT_STR);
         final CharTermAttribute termAtt = stream.getAttribute(CharTermAttribute.class);
         final TypeAttribute typeAtt = stream.getAttribute(TypeAttribute.class);
         final PositionIncrementAttribute pincAtt = stream.getAttribute(PositionIncrementAttribute.class);
@@ -70,17 +77,20 @@ public class SimpleAnalysisTest extends LuceneTestCase {
     @Test
     public void simpleTokenizerTest() throws Exception {
         UserDictionary userDictionary = null;
-        DecompoundMode decompoundMode = null;
+        DecompoundMode decompoundMode = DecompoundMode.MIXED;
         boolean discardPunctuation = false;
         Tokenizer tokenizer = null;
+        {
+            Reader reader = new StringReader(USER_DICT_STR);
+            userDictionary = UserDictionary.open(reader);
+        }
         try {
-            tokenizer = new KoreanTokenizer(KoreanTokenizer.DEFAULT_TOKEN_ATTRIBUTE_FACTORY, userDictionary,
-                decompoundMode, false, discardPunctuation);
-            Reader input = new StringReader(text);
+            tokenizer = createTokenizer(userDictionary, decompoundMode, discardPunctuation);
+            Reader input = new StringReader(TEXT_STR);
             tokenizer.setReader(input);
             tokenizer.reset();
             CharTermAttribute termAtt = tokenizer.addAttribute(CharTermAttribute.class);
-            while(tokenizer.incrementToken()) {
+            while (tokenizer.incrementToken()) {
                 logger.debug("TOKEN:{}", termAtt);
             }
         } finally {
@@ -88,6 +98,40 @@ public class SimpleAnalysisTest extends LuceneTestCase {
         }
         assertTrue(true);
     }
+
+    @Test
+    public void simpleAnalyzerTest() throws Exception {
+        UserDictionary userDictionary = null;
+        DecompoundMode decompoundMode = DecompoundMode.MIXED;
+        Set<Tag> stopTags = null;
+        {
+            Reader reader = new StringReader(USER_DICT_STR);
+            userDictionary = UserDictionary.open(reader);
+        }
+        {
+            stopTags = new HashSet<Tag>();
+            for (String tagStr : STOP_TAG_STR.split("[,]")) {
+                stopTags.add(POS.resolveTag(tagStr));
+            }
+        }
+        Analyzer analyzer = null;
+        TokenStream stream = null;
+        try {
+            Reader input = new StringReader(TEXT_STR);
+            analyzer = new KoreanAnalyzer(userDictionary, decompoundMode, stopTags, false);
+            stream = analyzer.tokenStream("", input);
+            stream.reset();
+            CharTermAttribute termAtt = stream.getAttribute(CharTermAttribute.class);
+            while (stream.incrementToken()) {
+                logger.debug("TOKEN:{}", termAtt);
+            }
+        } finally {
+            stream.close();
+            analyzer.close();
+        }
+        assertTrue(true);
+    }
+
 
     @Test
     public void createDictionaryTest() {
@@ -125,6 +169,14 @@ public class SimpleAnalysisTest extends LuceneTestCase {
             try { deleteDir(outputDir, deleteFilter); } catch (Exception ignore) { }
         }
         assertTrue(true);
+    }
+
+    public Tokenizer createTokenizer(
+        UserDictionary userDictionary,
+        DecompoundMode decompoundMode,
+        boolean discardPunctuation) {
+		return new KoreanTokenizer(KoreanTokenizer.DEFAULT_TOKEN_ATTRIBUTE_FACTORY, userDictionary,
+            decompoundMode, false, discardPunctuation);
     }
 
     public static File createTmpDir() throws IOException {
